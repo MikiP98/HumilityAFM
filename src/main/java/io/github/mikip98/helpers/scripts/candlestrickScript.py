13ba9a31798aaa8a6e0264bbb951cc24ef4eb072
unknown <pokora.mikolaj@gmail.com>
2023-12-22 20:02:10 +0100

    fixed candlestick model not loading

--- src/main/java/io/github/mikip98/helpers/scripts/candlestrickScript.py
+++ src/main/java/io/github/mikip98/helpers/scripts/candlestrickScript.py
@@ -4,9 +4,11 @@ def generateNames():
     from generateCabinetBlockVariantsJSONs import vanilla_wool_types
     cabinetBlockVariantsNames = []
     for metalType in metalTypes:
+        cabinetBlockVariantsNames.append("candlestick_" + metalType)
         cabinetBlockVariantsNames.append("candlestick_" + metalType + "_candle")
         for woolType in vanilla_wool_types:
-            cabinetBlockVariantsNames.append("candlestick_" + metalType + "_" + woolType)
+            cabinetBlockVariantsNames.append("candlestick_" + metalType + "_candle_" + woolType)
+    print(cabinetBlockVariantsNames)
     return cabinetBlockVariantsNames
 
 def generateJSONs():
@@ -46,30 +48,37 @@ def generateJSONs():
     for metalType in metalTypes:
         JSON = """
 {
-    "parent": "block/candlestick",
+    "parent": "humility-afm:block/candlestick",
     "textures": {
         "0": "block/""" + metalType + """_block",
         "particle": "block/""" + metalType + """_block"
     }
 }"""
-        with open("src/main/resources/assets/humility-afm/models/block/candlestick_" + metalType + "_candle.json", "w") as file:
+        with open("src/main/resources/assets/humility-afm/models/block/candlestick_" + metalType + ".json", "w") as file:
             file.write(JSON)
 
         for woolType in vanilla_wool_types:
             JSON = """
 {
-    "parent": "block/candlestick_candle",
+    "parent": "humility-afm:block/candlestick_candle",
     "textures": {
         "0": "block/""" + metalType + """_block",
         "1": "block/""" + woolType + """_wool",
         "particle": "block/""" + metalType + """_block"
     }
 }"""
-            with open("src/main/resources/assets/humility-afm/models/block/candlestick_" + metalType + "_" + woolType + ".json", "w") as file:
+            with open("src/main/resources/assets/humility-afm/models/block/candlestick_" + metalType + "_candle_" + woolType + ".json", "w") as file:
                 file.write(JSON)
 
     # Generate candlestick item models
     for metalType in metalTypes:
+        JSON = """
+{
+    "parent": "humility-afm:block/candlestick_""" + metalType + """"
+}"""
+        with open("src/main/resources/assets/humility-afm/models/item/candlestick_" + metalType + ".json", "w") as file:
+            file.write(JSON)
+
         JSON = """
 {
     "parent": "humility-afm:block/candlestick_""" + metalType + """_candle"
