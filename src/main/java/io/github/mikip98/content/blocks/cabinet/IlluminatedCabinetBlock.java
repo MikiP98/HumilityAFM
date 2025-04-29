7f0264c1e2d056cdee50acb5edd7241ac4aea450
MikiP98 <pokora.mikolaj@gmail.com>
2025-04-29 22:09:18 +0200

    Moved default Illuminated Cabinet block settings to its class

--- src/main/java/io/github/mikip98/content/blocks/cabinet/IlluminatedCabinetBlock.java
+++ src/main/java/io/github/mikip98/content/blocks/cabinet/IlluminatedCabinetBlock.java
@@ -1,5 +1,6 @@
 package io.github.mikip98.content.blocks.cabinet;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
@@ -7,6 +8,11 @@ import io.github.mikip98.content.blockentities.cabinetBlock.IlluminatedCabinetBl
 
 public class IlluminatedCabinetBlock extends CabinetBlock {
 
+    public static final FabricBlockSettings defaultSettings = FabricBlockSettings
+            .copyOf(CabinetBlock.defaultSettings)
+            .luminance(2);
+
+    public IlluminatedCabinetBlock() { super(defaultSettings); }
     public IlluminatedCabinetBlock(Settings settings) { super(settings); }
 
     @Override
