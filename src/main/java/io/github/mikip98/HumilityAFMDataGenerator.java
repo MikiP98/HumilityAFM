ceefc311082aef6ff77401d6a4ffa5e8602114f6
MikiP98 <pokora.mikolaj@gmail.com>
2025-04-29 21:59:14 +0200

    Significantly improved fabric datagen

--- src/main/java/io/github/mikip98/HumilityAFMDataGenerator.java
+++ src/main/java/io/github/mikip98/HumilityAFMDataGenerator.java
@@ -1,40 +1,25 @@
 package io.github.mikip98;
 
-import io.github.mikip98.helpers.CabinetBlockHelper;
+import io.github.mikip98.datagen.BlockLootTableGenerator;
+import io.github.mikip98.datagen.BlockTagGenerator;
+import io.github.mikip98.datagen.language.PolishLangProvider;
+import io.github.mikip98.datagen.language.UKEnglishLangProvider;
+import io.github.mikip98.datagen.language.USEnglishLangProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.minecraft.block.Block;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.TagKey;
-import net.minecraft.util.Identifier;
 
-import java.util.concurrent.CompletableFuture;
+import java.io.File;
 
 public class HumilityAFMDataGenerator implements DataGeneratorEntrypoint {
-
-	private static class MineableAxeTagGenerator extends FabricTagProvider.BlockTagProvider {
-		public MineableAxeTagGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
-			super(output, completableFuture);
-		}
-
-		private static final TagKey<Block> AXE_MINEABLE = TagKey.of(RegistryKeys.BLOCK, new Identifier("minecraft:mineable/axe"));
-
-		@Override
-		protected void configure(RegistryWrapper.WrapperLookup arg) {
-			for (Block block: CabinetBlockHelper.moddedCabinetBlockVariants) {
-				getOrCreateTagBuilder(AXE_MINEABLE).add(block);
-			}
-		}
-	}
-
-
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-		pack.addProvider(MineableAxeTagGenerator::new);
+		pack.addProvider(BlockTagGenerator::new);
+		pack.addProvider(BlockLootTableGenerator::new);
+		// Language providers
+		pack.addProvider(PolishLangProvider::new);
+		pack.addProvider(UKEnglishLangProvider::new);
+		pack.addProvider(USEnglishLangProvider::new);
 	}
 }
