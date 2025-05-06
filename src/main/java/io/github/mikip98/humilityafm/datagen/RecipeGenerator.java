81349046138e00e627da23f678b6476a89627532
MikiP98 <pokora.mikolaj@gmail.com>
2025-05-06 22:24:26 +0200

    Finished Fabric datagen for cabinet recipies; Removed old recipe files

--- src/main/java/io/github/mikip98/humilityafm/datagen/RecipeGenerator.java
+++ src/main/java/io/github/mikip98/humilityafm/datagen/RecipeGenerator.java
@@ -1,17 +1,20 @@
 package io.github.mikip98.humilityafm.datagen;
 
 import io.github.mikip98.humilityafm.generators.CabinetBlockGenerator;
-import io.github.mikip98.humilityafm.helpers.MainHelper;
 import io.github.mikip98.humilityafm.registries.BlockRegistry;
+import io.github.mikip98.humilityafm.util.GenerationData;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
@@ -26,7 +29,16 @@ public class RecipeGenerator extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
+        // ............ TEST BLOCKS & BLOCK ITEMS ............
+
         // Cabinet Blocks
+        offerCabinetRecipe(
+                exporter,
+                BlockRegistry.CABINET_BLOCK,
+                Items.PETRIFIED_OAK_SLAB,
+                Items.WHITE_CARPET,
+                "cabinets/"
+        );
 
         // Illuminated Cabinet Blocks
         offerIlluminatedCabinetRecipe(
@@ -34,91 +46,92 @@ public class RecipeGenerator extends FabricRecipeProvider {
                 BlockRegistry.ILLUMINATED_CABINET_BLOCK,
                 BlockRegistry.CABINET_BLOCK
         );
-        final Map<String, Item> color2DyeMap = new HashMap<>();
-        color2DyeMap.put("white", Items.WHITE_DYE);
-        color2DyeMap.put("orange", Items.ORANGE_DYE);
-        color2DyeMap.put("magenta", Items.MAGENTA_DYE);
-        color2DyeMap.put("light_blue", Items.LIGHT_BLUE_DYE);
-        color2DyeMap.put("yellow", Items.YELLOW_DYE);
-        color2DyeMap.put("lime", Items.LIME_DYE);
-        color2DyeMap.put("pink", Items.PINK_DYE);
-        color2DyeMap.put("gray", Items.GRAY_DYE);
-        color2DyeMap.put("light_gray", Items.LIGHT_GRAY_DYE);
-        color2DyeMap.put("cyan", Items.CYAN_DYE);
-        color2DyeMap.put("purple", Items.PURPLE_DYE);
-        color2DyeMap.put("blue", Items.BLUE_DYE);
-        color2DyeMap.put("brown", Items.BROWN_DYE);
-        color2DyeMap.put("green", Items.GREEN_DYE);
-        color2DyeMap.put("red", Items.RED_DYE);
-        color2DyeMap.put("black", Items.BLACK_DYE);
-//        for (int i = 0; i < CabinetBlockHelper.cabinetBlockVariantsNames.length; i++) {
-//            IlluminatedCabinetBlock illuminatedCabinetBlock = (IlluminatedCabinetBlock) CabinetBlockHelper.illuminatedCabinetBlockVariants[i];
-//            CabinetBlock cabinetBlock = (CabinetBlock) CabinetBlockHelper.cabinetBlockVariants[i];
-//            String[] parts = Parts.getParts(illuminatedCabinetBlock);
-//
-//            offerIlluminatedCabinetRecipe(
-//                    exporter,
-//                    illuminatedCabinetBlock,
-//                    cabinetBlock
-//            );
-//
-//            offerColorChangeRecipie(
-//                    exporter,
-//                    cabinetBlock,
-//                    ModItemTags.,
-//                    color2DyeMap.get(C),
-//                    MOD_ID + "/cabinets"
-//            );
-//
-//            offerColorChangeRecipie(
-//                    exporter,
-//                    illuminatedCabinetBlock,
-//                    ModItemTags.ILLUMINATED_CABINET_BLOCKS,
-//                    Items.GLOW_INK_SAC,
-//                    MOD_ID + "/illuminated_cabinets"
-//            );
-//        }
 
-        short startIndex = CabinetBlockGenerator.woodTypeToStartIndex.get("oak");
+        // ............ FINAL BLOCKS & BLOCK ITEMS ............
+        generateCabinetRecipies(exporter);
+    }
+
+    protected static void generateCabinetRecipies(Consumer<RecipeJsonProvider> exporter) {
         short i = 0;
-        List<Item> oakCabinets = List.of(Arrays.copyOfRange(CabinetBlockGenerator.cabinetBlockItemVariants, startIndex, startIndex + MainHelper.vanillaWoolTypes.length));
-        for (String color : MainHelper.vanillaWoolTypes) {
-            Item cabinetBlockItemVariant = CabinetBlockGenerator.cabinetBlockItemVariants[i];
-            Item illuminatedCabinetBlockItemVariant = CabinetBlockGenerator.illuminatedCabinetBlockItemVariants[i];
+        for (String woodType : GenerationData.vanillaWoodTypes) {
+            Item[] currentWoodTypeCabinets = Arrays.copyOfRange(CabinetBlockGenerator.cabinetBlockItemVariants, i, i + GenerationData.vanillaColorPallet.length);
+            Item[] currentWoodTypeIlluminatedCabinets = Arrays.copyOfRange(CabinetBlockGenerator.illuminatedCabinetBlockItemVariants, i, i + GenerationData.vanillaColorPallet.length);
 
+            for (String color : GenerationData.vanillaColorPallet) {
+                Item cabinetBlockItemVariant = CabinetBlockGenerator.cabinetBlockItemVariants[i];
+                Item illuminatedCabinetBlockItemVariant = CabinetBlockGenerator.illuminatedCabinetBlockItemVariants[i];
 
-            List<Item> oakCabinets2 = new ArrayList<>(oakCabinets);
-            oakCabinets2.remove(i);
-            Ingredient otherOakCabinets = Ingredient.ofItems(oakCabinets2.toArray(new Item[0]));
-            offerColorChangeRecipie(
-                    exporter,
-                    cabinetBlockItemVariant,
-                    otherOakCabinets,
-                    color2DyeMap.get(color),
-                    MOD_ID + "/cabinets"
-            );
+                offerCabinetRecipe(
+                        exporter,
+                        cabinetBlockItemVariant,
+                        getItemFromName(woodType + "_slab"),
+                        getItemFromName(color + "_carpet"),
+                        "cabinets/"
+                );
 
-            offerColorChangeRecipie(
-                    exporter,
-                    illuminatedCabinetBlockItemVariant,
-                    otherOakCabinets,
-                    color2DyeMap.get(color),
-                    MOD_ID + "/illuminated_cabinets"
-            );
+                offerIlluminatedCabinetRecipe(
+                        exporter,
+                        illuminatedCabinetBlockItemVariant,
+                        cabinetBlockItemVariant
+                );
 
-            ++i;
+                // Color change recipies
+                Item currentDye = getItemFromName(color + "_dye");
+
+                List<Item> currentWoodTypeCabinetsWithoutCurrentColor = new ArrayList<>(List.of(currentWoodTypeCabinets));
+                currentWoodTypeCabinetsWithoutCurrentColor.remove(i % GenerationData.vanillaColorPallet.length);
+
+                offerColorChangeRecipie(
+                        exporter,
+                        cabinetBlockItemVariant,
+                        Ingredient.ofItems(currentWoodTypeCabinetsWithoutCurrentColor.toArray(new Item[0])),
+                        currentDye,
+                        MOD_ID + "/cabinets",
+                        "cabinets/color_change/"
+                );
+
+                List<Item> currentWoodTypeIlluminatedCabinetsWithoutCurrentColor = new ArrayList<>(List.of(currentWoodTypeIlluminatedCabinets));
+                currentWoodTypeIlluminatedCabinetsWithoutCurrentColor.remove(i % GenerationData.vanillaColorPallet.length);
+
+                offerColorChangeRecipie(
+                        exporter,
+                        illuminatedCabinetBlockItemVariant,
+                        Ingredient.ofItems(currentWoodTypeIlluminatedCabinetsWithoutCurrentColor.toArray(new Item[0])),
+                        currentDye,
+                        MOD_ID + "/illuminated_cabinets",
+                        "illuminated_cabinets/color_change/"
+                );
+
+                ++i;
+            }
         }
     }
 
 
-    protected static void offerColorChangeRecipie(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, Ingredient input, ItemConvertible dye, String group) {
+    protected static void offerCabinetRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible slab, ItemConvertible carpet, String path_prefix) {
+        ShapedRecipeJsonBuilder
+                .create(RecipeCategory.MISC, output, 1)
+                .pattern(" G ")
+                .pattern("SCS")
+                .pattern(" S ")
+                .input('G', Items.GLASS_PANE)
+                .input('S', slab)
+                .input('C', carpet)
+                .group(MOD_ID + "/cabinets")
+                .criterion(hasItem(Items.GLASS_PANE), conditionsFromItem(Items.GLASS_PANE))
+                .criterion(hasItem(slab), conditionsFromItem(slab))
+                .criterion(hasItem(carpet), conditionsFromItem(carpet))
+                .offerTo(exporter, path_prefix + getRecipeName(output));
+    }
+
+    protected static void offerColorChangeRecipie(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, Ingredient input, ItemConvertible dye, String group, String path_prefix) {
         ShapelessRecipeJsonBuilder
                 .create(RecipeCategory.MISC, output, 1)
                 .input(input)
                 .input(dye)
                 .group(group)
                 .criterion(hasItem(dye), conditionsFromItem(dye))
-                .offerTo(exporter, getRecipeName(output) + "_color_change");
+                .offerTo(exporter, path_prefix + getRecipeName(output) + "_color_change");
     }
 
     protected static void offerIlluminatedCabinetRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible illuminated_cabinet, ItemConvertible cabinet) {
@@ -128,11 +141,12 @@ public class RecipeGenerator extends FabricRecipeProvider {
                 cabinet,
                 Items.GLOW_INK_SAC,
                 MOD_ID + "/illuminated_cabinets",
-                1
+                1,
+                "illuminated_cabinets/"
         );
     }
 
-    protected static void offerDoubleInputShapelessRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input1, ItemConvertible input2, @Nullable String group, int outputCount) {
+    protected static void offerDoubleInputShapelessRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input1, ItemConvertible input2, @Nullable String group, int outputCount, String path_prefix) {
         LOGGER.info("Generating recipe with id: {}", getRecipeName(output));
         ShapelessRecipeJsonBuilder
                 .create(RecipeCategory.MISC, output, outputCount)
@@ -141,6 +155,11 @@ public class RecipeGenerator extends FabricRecipeProvider {
                 .group(group)
                 .criterion(hasItem(input1), conditionsFromItem(input1))
                 .criterion(hasItem(input2), conditionsFromItem(input2))
-                .offerTo(exporter, getRecipeName(output));
+                .offerTo(exporter, path_prefix + getRecipeName(output));
+    }
+
+
+    protected static Item getItemFromName(String name) {
+        return Registries.ITEM.get(new Identifier(name));
     }
 }
