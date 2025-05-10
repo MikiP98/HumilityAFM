27995178f05e64dc9a17364cc59013f9d5b3f25e
MikiP98 <pokora.mikolaj@gmail.com>
2025-05-10 14:37:46 +0200

    Added Fabric model and blockstate datagen for wooden forced corner stairs

--- src/main/java/io/github/mikip98/humilityafm/datagen/ModelGenerator.java
+++ src/main/java/io/github/mikip98/humilityafm/datagen/ModelGenerator.java
@@ -1,6 +1,7 @@
 package io.github.mikip98.humilityafm.datagen;
 
 import io.github.mikip98.humilityafm.generators.CabinetBlockGenerator;
+import io.github.mikip98.humilityafm.generators.ForcedCornerStairsGenerator;
 import io.github.mikip98.humilityafm.helpers.TerracottaTilesHelper;
 import io.github.mikip98.humilityafm.helpers.WoodenMosaicHelper;
 import io.github.mikip98.humilityafm.registries.BlockRegistry;
@@ -8,6 +9,7 @@ import io.github.mikip98.humilityafm.util.GenerationData;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.block.Block;
+import net.minecraft.block.enums.BlockHalf;
 import net.minecraft.data.client.*;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
@@ -23,13 +25,22 @@ public class ModelGenerator extends FabricModelProvider {
             Optional.of(getId("block/cabinet_block")),
             Optional.empty()
     );
-    protected static final Model CABINET_BLOCK_OPEN = new Model(
+    protected static final Model CABINET_BLOCK_OPEN_MODEL = new Model(
             Optional.of(getId("block/cabinet_block_open")),
             Optional.empty()
     );
     // Checker 2x2 Model
-    protected static final Model CHECKER_2X2 = new Model(
+    protected static final Model CHECKER_2X2_MODEL = new Model(
             Optional.of(getId("block/checker_2x2")),
+            Optional.empty()
+    );
+    // Forced Corner Stairs Models
+    protected static final Model INNER_CORNER_STAIRS_MODEL = new Model(
+            Optional.of(getId("block/stairs_inner")),
+            Optional.empty()
+    );
+    protected static final Model OUTER_CORNER_STAIRS_MODEL = new Model(
+            Optional.of(getId("block/stairs_outer")),
             Optional.empty()
     );
 
@@ -50,6 +61,11 @@ public class ModelGenerator extends FabricModelProvider {
         // Wooden Mosaic
         blockStateModelGenerator.registerParentedItemModel(BlockRegistry.WOODEN_MOSAIC, getId("block/checker_2x2"));
         blockStateModelGenerator.blockStateCollector.accept(getDefaultBlockstate(BlockRegistry.WOODEN_MOSAIC, getId("block/checker_2x2")));
+        // Forced Corner Stairs
+        blockStateModelGenerator.registerParentedItemModel(BlockRegistry.INNER_STAIRS, getId("block/stairs_inner"));
+        blockStateModelGenerator.registerParentedItemModel(BlockRegistry.OUTER_STAIRS, getId("block/stairs_outer"));
+        blockStateModelGenerator.blockStateCollector.accept(getForcedCornerStairsBlockstate(BlockRegistry.INNER_STAIRS, getId("block/stairs_inner")));
+        blockStateModelGenerator.blockStateCollector.accept(getForcedCornerStairsBlockstate(BlockRegistry.OUTER_STAIRS, getId("block/stairs_outer")));
 
         // ............ FINAL BLOCKS & BLOCK ITEMS ............
         generateCabinetModelsAndBlockStates(blockStateModelGenerator);
@@ -59,7 +75,34 @@ public class ModelGenerator extends FabricModelProvider {
     }
 
     protected static void generateForcedCornerStairsModelsAndBlockstates(BlockStateModelGenerator blockStateModelGenerator) {
+        int i = 0;
+        for (String woodType : GenerationData.vanillaWoodTypes) {
+            final Identifier innerStairsModelId = INNER_CORNER_STAIRS_MODEL.upload(
+                    getId("block/corner_stairs/inner_stairs/inner_stairs_" + woodType),
+                    new TextureMap()
+                            .register(TextureKey.PARTICLE, new Identifier("block/" + woodType + "_planks"))
+                            .register(TextureKey.TOP, new Identifier("block/" + woodType + "_planks"))
+                            .register(TextureKey.BOTTOM, new Identifier("block/" + woodType + "_planks"))
+                            .register(TextureKey.SIDE, new Identifier("block/" + woodType + "_planks")),
+                    blockStateModelGenerator.modelCollector
+            );
+            final Identifier outerStairsModelId = OUTER_CORNER_STAIRS_MODEL.upload(
+                    getId("block/corner_stairs/outer_stairs/outer_stairs_" + woodType),
+                    new TextureMap()
+                            .register(TextureKey.PARTICLE, new Identifier("block/" + woodType + "_planks"))
+                            .register(TextureKey.TOP, new Identifier("block/" + woodType + "_planks"))
+                            .register(TextureKey.BOTTOM, new Identifier("block/" + woodType + "_planks"))
+                            .register(TextureKey.SIDE, new Identifier("block/" + woodType + "_planks")),
+                    blockStateModelGenerator.modelCollector
+            );
 
+            blockStateModelGenerator.registerParentedItemModel(ForcedCornerStairsGenerator.innerStairsBlockVariants[i], innerStairsModelId);
+            blockStateModelGenerator.registerParentedItemModel(ForcedCornerStairsGenerator.outerStairsBlockVariants[i], outerStairsModelId);
+            blockStateModelGenerator.blockStateCollector.accept(getForcedCornerStairsBlockstate(ForcedCornerStairsGenerator.innerStairsBlockVariants[i], innerStairsModelId));
+            blockStateModelGenerator.blockStateCollector.accept(getForcedCornerStairsBlockstate(ForcedCornerStairsGenerator.outerStairsBlockVariants[i], outerStairsModelId));
+
+            ++i;
+        }
     }
 
     protected static void generateWoodenMosaicModelsAndBlockStates(BlockStateModelGenerator blockStateModelGenerator) {
@@ -68,7 +111,7 @@ public class ModelGenerator extends FabricModelProvider {
             for (String woodType2 : GenerationData.vanillaWoodTypes) {
                 if (woodType.equals(woodType2)) continue;
 
-                final Identifier woodenMosaicModelId = CHECKER_2X2.upload(
+                final Identifier woodenMosaicModelId = CHECKER_2X2_MODEL.upload(
                         getId("block/wooden_mosaic/wooden_mosaic_" + woodType + "_" + woodType2),
                         new TextureMap()
                                 .register(TextureKey.of("1"), new Identifier("block/" + woodType + "_planks"))
@@ -89,7 +132,7 @@ public class ModelGenerator extends FabricModelProvider {
             for (String color2 : GenerationData.vanillaColorPallet) {
                 if (color.equals(color2)) continue;
 
-                final Identifier terracottaTileModelId = CHECKER_2X2.upload(
+                final Identifier terracottaTileModelId = CHECKER_2X2_MODEL.upload(
                         getId("block/terracotta_tiles/terracotta_tiles_" + color + "_" + color2),
                         new TextureMap()
                                 .register(TextureKey.of("1"), new Identifier("block/" + color + "_terracotta"))
@@ -120,7 +163,7 @@ public class ModelGenerator extends FabricModelProvider {
                     Optional.empty()
             );
 
-            final Identifier woodTypeCabinetOpenId = CABINET_BLOCK_OPEN.upload(
+            final Identifier woodTypeCabinetOpenId = CABINET_BLOCK_OPEN_MODEL.upload(
                     getId("block/cabinet/open/cabinet_block_open_" + woodType),
                     new TextureMap()
                             .register(TextureKey.of("2"), new Identifier("block/" + woodType + "_planks"))
@@ -153,6 +196,71 @@ public class ModelGenerator extends FabricModelProvider {
         }
     }
 
+
+    protected static VariantsBlockStateSupplier getForcedCornerStairsBlockstate(Block block, Identifier modelId) {
+        return VariantsBlockStateSupplier
+                .create(block)
+                .coordinate(BlockStateVariantMap
+                        .create(Properties.HORIZONTAL_FACING, Properties.BLOCK_HALF)
+                        .register(
+                                Direction.EAST, BlockHalf.BOTTOM,
+                                BlockStateVariant.create()
+                                        .put(VariantSettings.MODEL, modelId)
+                                        .put(VariantSettings.UVLOCK, true)
+                                        .put(VariantSettings.Y, VariantSettings.Rotation.R90)
+                        )
+                        .register(
+                                Direction.EAST, BlockHalf.TOP,
+                                BlockStateVariant.create()
+                                        .put(VariantSettings.MODEL, modelId)
+                                        .put(VariantSettings.UVLOCK, true)
+                                        .put(VariantSettings.Y, VariantSettings.Rotation.R180)
+                                        .put(VariantSettings.X, VariantSettings.Rotation.R180)
+                        )
+                        .register(
+                                Direction.NORTH, BlockHalf.BOTTOM,
+                                BlockStateVariant.create()
+                                        .put(VariantSettings.MODEL, modelId)
+                        )
+                        .register(
+                                Direction.NORTH, BlockHalf.TOP,
+                                BlockStateVariant.create()
+                                        .put(VariantSettings.MODEL, modelId)
+                                        .put(VariantSettings.UVLOCK, true)
+                                        .put(VariantSettings.Y, VariantSettings.Rotation.R90)
+                                        .put(VariantSettings.X, VariantSettings.Rotation.R180)
+                        )
+                        .register(
+                                Direction.SOUTH, BlockHalf.BOTTOM,
+                                BlockStateVariant.create()
+                                        .put(VariantSettings.MODEL, modelId)
+                                        .put(VariantSettings.UVLOCK, true)
+                                        .put(VariantSettings.Y, VariantSettings.Rotation.R180)
+                        )
+                        .register(
+                                Direction.SOUTH, BlockHalf.TOP,
+                                BlockStateVariant.create()
+                                        .put(VariantSettings.MODEL, modelId)
+                                        .put(VariantSettings.UVLOCK, true)
+                                        .put(VariantSettings.Y, VariantSettings.Rotation.R270)
+                                        .put(VariantSettings.X, VariantSettings.Rotation.R180)
+                        )
+                        .register(
+                                Direction.WEST, BlockHalf.BOTTOM,
+                                BlockStateVariant.create()
+                                        .put(VariantSettings.MODEL, modelId)
+                                        .put(VariantSettings.UVLOCK, true)
+                                        .put(VariantSettings.Y, VariantSettings.Rotation.R270)
+                        )
+                        .register(
+                                Direction.WEST, BlockHalf.TOP,
+                                BlockStateVariant.create()
+                                        .put(VariantSettings.MODEL, modelId)
+                                        .put(VariantSettings.UVLOCK, true)
+                                        .put(VariantSettings.X, VariantSettings.Rotation.R180)
+                        )
+                );
+    }
 
     protected static VariantsBlockStateSupplier getDefaultBlockstate(Block block, Identifier modelId) {
         return VariantsBlockStateSupplier.create(block, BlockStateVariant.create().put(VariantSettings.MODEL, modelId));
