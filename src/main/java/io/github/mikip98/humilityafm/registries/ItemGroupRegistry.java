21a8e26d597425f9d5fd726fd58537c7e4ede2c2
MikiP98 <pokora.mikolaj@gmail.com>
2025-05-13 18:59:23 +0200

    Various reworks and improvements

--- src/main/java/io/github/mikip98/humilityafm/registries/ItemGroupRegistry.java
+++ src/main/java/io/github/mikip98/humilityafm/registries/ItemGroupRegistry.java
@@ -2,14 +2,21 @@ package io.github.mikip98.humilityafm.registries;
 
 import io.github.mikip98.humilityafm.config.ModConfig;
 import io.github.mikip98.humilityafm.generators.CabinetBlockGenerator;
+import io.github.mikip98.humilityafm.generators.CandlestickGenerator;
+import io.github.mikip98.humilityafm.generators.ColouredLightsGenerator;
 import io.github.mikip98.humilityafm.generators.ForcedCornerStairsGenerator;
 import io.github.mikip98.humilityafm.helpers.*;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.text.Text;
+
+import java.util.Arrays;
 
 import static io.github.mikip98.humilityafm.HumilityAFM.getId;
 
@@ -19,12 +26,8 @@ public class ItemGroupRegistry {
             .icon(() -> new ItemStack(BlockRegistry.CABINET_BLOCK))
             .displayName(Text.translatable("itemGroup.cabinets"))
             .entries((displayContext, entries) -> {
-                for (int i = 0; i < CabinetBlockGenerator.cabinetBlockVariants.length; i++) {
-                    entries.add(new ItemStack(CabinetBlockGenerator.cabinetBlockVariants[i]));
-                }
-                for (int i = 0; i < CabinetBlockGenerator.illuminatedCabinetBlockVariants.length; i++) {
-                    entries.add(new ItemStack(CabinetBlockGenerator.illuminatedCabinetBlockVariants[i]));
-                }
+                Arrays.stream(CabinetBlockGenerator.cabinetBlockVariants).forEach(entries::add);
+                Arrays.stream(CabinetBlockGenerator.illuminatedCabinetBlockVariants).forEach(entries::add);
             })
             .build();
 
@@ -33,12 +36,8 @@ public class ItemGroupRegistry {
             .icon(() -> new ItemStack(BlockRegistry.OUTER_STAIRS))
             .displayName(Text.translatable("itemGroup.innerOuterStairs"))
             .entries((displayContext, entries) -> {
-                for (int i = 0; i < ForcedCornerStairsGenerator.innerStairsBlockVariants.length; i++) {
-                    entries.add(new ItemStack(ForcedCornerStairsGenerator.innerStairsBlockVariants[i]));
-                }
-                for (int i = 0; i < ForcedCornerStairsGenerator.outerStairsBlockVariants.length; i++) {
-                    entries.add(new ItemStack(ForcedCornerStairsGenerator.outerStairsBlockVariants[i]));
-                }
+                Arrays.stream(ForcedCornerStairsGenerator.innerStairsBlockVariants).forEach(entries::add);
+                Arrays.stream(ForcedCornerStairsGenerator.outerStairsBlockVariants).forEach(entries::add);
             })
             .build();
 
@@ -47,10 +46,7 @@ public class ItemGroupRegistry {
             .icon(() -> new ItemStack(WoodenMosaicHelper.woodenMosaicVariants[0]))
             .displayName(Text.translatable("itemGroup.woodenMosaics"))
             .entries((displayContext, entries) -> {
-                for (int i = 0; i < WoodenMosaicHelper.woodenMosaicVariants.length; i++) {
-//						LOGGER.info("Adding wooden mosaic variant to item group: " + WoodenMosaicHelper.woodenMosaicVariants[i]);
-                    entries.add(new ItemStack(WoodenMosaicHelper.woodenMosaicVariants[i])); // TODO: Fix this
-                }
+                Arrays.stream(WoodenMosaicHelper.woodenMosaicVariants).forEach(entries::add);
             })
             .build();
 
@@ -59,10 +55,7 @@ public class ItemGroupRegistry {
             .icon(() -> new ItemStack(TerracottaTilesHelper.terracottaTilesVariants[0]))
             .displayName(Text.translatable("itemGroup.terracottaTiles"))
             .entries((displayContext, entries) -> {
-                for (int i = 0; i < TerracottaTilesHelper.terracottaTilesVariants.length; i++) {
-//						LOGGER.info("Adding terracotta tiles variant to item group: " + TerracottaTilesHelper.terracottaTilesVariants[i]);
-                    entries.add(new ItemStack(TerracottaTilesHelper.terracottaTilesVariants[i]));
-                }
+                Arrays.stream(TerracottaTilesHelper.terracottaTilesVariants).forEach(entries::add);
             })
             .build();
 
@@ -71,49 +64,15 @@ public class ItemGroupRegistry {
             .icon(() -> new ItemStack(PumpkinHelper.PumpkinsVariants[0]))
             .displayName(Text.translatable("itemGroup.humilityMisc"))
             .entries((displayContext, entries) -> {
-                for (int i = 0; i < PumpkinHelper.PumpkinsVariants.length; i++) {
-                    entries.add(new ItemStack(PumpkinHelper.PumpkinsVariants[i]));
+                Arrays.stream(PumpkinHelper.PumpkinsVariants).forEach(entries::add);
+                Arrays.stream(ItemRegistry.glowingPowderVariants).forEach(entries::add);
+                Arrays.stream(ColouredLightsGenerator.colouredTorchesVariants).forEach(entries::add);
+                if (ModConfig.enableLEDs) Arrays.stream(ColouredLightsGenerator.LEDBlockVariants).forEach(entries::add);
+                if (ModConfig.enableCandlesticks) {
+                    Arrays.stream(CandlestickGenerator.candlestickClassicItemVariants).forEach(entries::add);
+                    CandlestickGenerator.candlestickRustableItemVariants.forEach(set -> Arrays.stream(set).forEach(entries::add));
                 }
-            })
-            .build();
-
-
-    final static ItemGroup LED_ITEM_GROUP;
-    final static ItemGroup CANDLESTICK_ITEM_GROUP;
-    static {
-        // LED item group
-        if (ModConfig.enableLEDs) {
-            LED_ITEM_GROUP = FabricItemGroup.builder()
-                    .icon(() -> new ItemStack(LEDHelper.LEDBlockVariants[0]))
-                    .displayName(Text.translatable("itemGroup.leds"))
-                    .entries((displayContext, entries) -> {
-                        for (int i = 0; i < LEDHelper.LEDBlockVariants.length; i++) {
-                            entries.add(new ItemStack(LEDHelper.LEDBlockVariants[i]));
-                        }
-                        for (int i = 0; i < LEDHelper.LEDPowderVariants.length; i++) {
-                            entries.add(new ItemStack(LEDHelper.LEDPowderVariants[i]));
-                        }
-                    })
-                    .build();
-        } else {
-            LED_ITEM_GROUP = null; // Initialize to null if LEDs are disabled
-        }
-
-        // Candlestick item group
-        if (ModConfig.enableCandlesticks) {
-            CANDLESTICK_ITEM_GROUP = FabricItemGroup.builder()
-                .icon(() -> new ItemStack(CandlestickHelper.candlestickVariants[0]))
-                .displayName(Text.translatable("itemGroup.candlesticks"))
-                .entries((displayContext, entries) -> {
-                    for (int i = 0; i < CandlestickHelper.candlestickVariants.length; i++) {
-                        entries.add(new ItemStack(CandlestickHelper.candlestickVariants[i]));
-                    }
-                })
-                .build();
-        } else {
-            CANDLESTICK_ITEM_GROUP = null; // Initialize to null if Candlesticks are disabled
-        }
-    }
+            }).build();
 
 
     public static void registerItemGroups() {
@@ -121,12 +80,20 @@ public class ItemGroupRegistry {
         Registry.register(Registries.ITEM_GROUP, getId("inner_outer_stairs_group"), INNER_OUTER_STAIRS_ITEM_GROUP);
         Registry.register(Registries.ITEM_GROUP, getId("wooden_mosaics_group"), WOODEN_MOSAIC_ITEM_GROUP);
         Registry.register(Registries.ITEM_GROUP, getId("terracotta_tiles_group"), TERRACOTTA_TILES_ITEM_GROUP);
-        if (ModConfig.enableLEDs) {
-            Registry.register(Registries.ITEM_GROUP, getId("leds_group"), LED_ITEM_GROUP);
-        }
-        if (ModConfig.enableCandlesticks) {
-            Registry.register(Registries.ITEM_GROUP, getId("candlesticks_group"), CANDLESTICK_ITEM_GROUP);
-        }
         Registry.register(Registries.ITEM_GROUP, getId("humility_misc_group"), HUMILITY_MISCELLANEOUS_GROUP);
+    }
+
+
+    public static void putIntoItemGroup(Item[] items, RegistryKey<ItemGroup> group) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(content -> {
+            for (Item item : items) {
+                content.add(item);
+            }
+        });
+    }
+    public static void putIntoItemGroup(Item item, RegistryKey<ItemGroup> group) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(content -> {
+            content.add(item);
+        });
     }
 }
