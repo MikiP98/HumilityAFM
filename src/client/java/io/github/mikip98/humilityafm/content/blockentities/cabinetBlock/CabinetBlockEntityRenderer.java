a0114afd0a25c050ebf31c49371cf758d0eaaef4
MikiP98 <pokora.mikolaj@gmail.com>
2025-05-16 14:58:54 +0200

    Removed unused code and fixed a rendering error

--- src/client/java/io/github/mikip98/humilityafm/content/blockentities/cabinetBlock/CabinetBlockEntityRenderer.java
+++ src/client/java/io/github/mikip98/humilityafm/content/blockentities/cabinetBlock/CabinetBlockEntityRenderer.java
@@ -3,6 +3,7 @@ package io.github.mikip98.humilityafm.content.blockentities.cabinetBlock;
 import io.github.mikip98.humilityafm.content.blocks.cabinet.CabinetBlock;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
@@ -12,29 +13,34 @@ import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.joml.Quaternionf;
-
-
-import static io.github.mikip98.humilityafm.HumilityAFM.LOGGER;
 
 @Environment(EnvType.CLIENT)
 public class CabinetBlockEntityRenderer implements BlockEntityRenderer<CabinetBlockEntity> {
-    // A jukebox itemstack
-    //private static ItemStack stack = new ItemStack(Items.JUKEBOX, 1);
-
-//    private static ArrayList<Item> blocks = new ArrayList<Item>();
-//    blocks.add(Items.BONE_BLOCK);
-//    blocks.add(Items.BRAIN_CORAL_BLOCK);
-//    blocks.add(Items.BROWN_MUSHROOM_BLOCK);
-//    blocks.size()
-
+    @SuppressWarnings("unused")
     public CabinetBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {}
 
     @Override
     public void render(CabinetBlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        // Check if block entity is null
-
         matrices.push();
+
+        World world = blockEntity.getWorld();
+        BlockPos pos = blockEntity.getPos();
+
+        if (world == null || pos == null) {
+            // If the world or position is null, return early
+            matrices.pop();
+            return;
+        }
+
+        BlockState blockState = world.getBlockState(pos);
+
+        if (blockState == null || !(blockState.getBlock() instanceof CabinetBlock)) {
+            matrices.pop();
+            return;
+        }
 
         final ItemStack stack = blockEntity.getStack(0);
 
@@ -46,38 +52,31 @@ public class CabinetBlockEntityRenderer implements BlockEntityRenderer<CabinetBl
         if (stack.getItem() instanceof BlockItem) {
             scaleY = 0.3125f;
         }
-        try {
-            switch (blockEntity.getWorld().getBlockState(blockEntity.getPos()).get(CabinetBlock.FACING)) {
-                case NORTH -> {
-                    matrices.translate(0.5, scaleY, 0.9);
+        switch (blockState.get(CabinetBlock.FACING)) {
+            case NORTH -> {
+                matrices.translate(0.5, scaleY, 0.9);
 
-                    rotation = new Quaternionf().rotationYXZ((float) Math.toRadians(180), 0.0f, 0.0f); // Create a rotation quaternion for a 180-degree rotation around the Y-axis
-                    matrices.multiply(rotation);
-                }
-                case SOUTH -> matrices.translate(0.5, scaleY, 0.1);
-                case EAST -> {
-                    matrices.translate(0.1, scaleY, 0.5);
-                    rotation = new Quaternionf().rotationYXZ((float) Math.toRadians(90), 0.0f, 0.0f);
-                    matrices.multiply(rotation);
-                }
-                case WEST -> {
-                    matrices.translate(0.9, scaleY, 0.5);
-                    rotation = new Quaternionf().rotationYXZ((float) Math.toRadians(270), 0.0f, 0.0f);
-                    matrices.multiply(rotation);
-                }
+                rotation = new Quaternionf().rotationYXZ((float) Math.toRadians(180), 0.0f, 0.0f); // Create a rotation quaternion for a 180-degree rotation around the Y-axis
+                matrices.multiply(rotation);
             }
-        } catch (Exception e) {
-            matrices.translate(0.5, scaleY, 0.1);
-            LOGGER.error("Error rendering cabinet block entity");
+            case SOUTH -> matrices.translate(0.5, scaleY, 0.1);
+            case EAST -> {
+                matrices.translate(0.1, scaleY, 0.5);
+                rotation = new Quaternionf().rotationYXZ((float) Math.toRadians(90), 0.0f, 0.0f);
+                matrices.multiply(rotation);
+            }
+            case WEST -> {
+                matrices.translate(0.9, scaleY, 0.5);
+                rotation = new Quaternionf().rotationYXZ((float) Math.toRadians(270), 0.0f, 0.0f);
+                matrices.multiply(rotation);
+            }
         }
-        //LOGGER.info("works");
 
-        //Actually render the item
-        //MinecraftClient.getInstance().getItemRenderer().renderItem(new ItemStack(Items.JUKEBOX, 1), ModelTransformation.Mode.GROUND, light, overlay, matrices, vertexConsumers, 0);
+        // Actually render the item
+        // MinecraftClient.getInstance().getItemRenderer().renderItem(new ItemStack(Items.JUKEBOX, 1), ModelTransformation.Mode.GROUND, light, overlay, matrices, vertexConsumers, 0);
         final BakedModel model = MinecraftClient.getInstance().getItemRenderer().getModel(stack, null, null, 0);
         MinecraftClient.getInstance().getItemRenderer().renderItem(stack, ModelTransformationMode.GROUND, false, matrices, vertexConsumers, light, overlay, model);
-        //15728640 + 255 = fully lit
-        //LOGGER.info("Light level: " + light);
+        // 15728640 + 255 = fully lit
 
         // Mandatory call after GL calls
         matrices.pop();
