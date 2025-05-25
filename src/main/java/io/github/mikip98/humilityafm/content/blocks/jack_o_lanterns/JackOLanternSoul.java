727f8b849e515567c236b17a24c9c9005b271766
MikiP98 <pokora.mikolaj@gmail.com>
2025-05-25 23:44:11 +0200

    Made Soul Jack o'Lantern emmit some particles

--- src/main/java/io/github/mikip98/humilityafm/content/blocks/jack_o_lanterns/JackOLanternSoul.java
+++ src/main/java/io/github/mikip98/humilityafm/content/blocks/jack_o_lanterns/JackOLanternSoul.java
@@ -1,9 +1,36 @@
 package io.github.mikip98.humilityafm.content.blocks.jack_o_lanterns;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
 
 public class JackOLanternSoul extends JackOLantern {
     public JackOLanternSoul() {
         super(FabricBlockSettings.copyOf(defaultSettings).luminance(8));
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (random.nextInt(4) == 0) {
+            world.addParticle(
+                    ParticleTypes.SOUL,
+                    pos.getX() + 0.5 + (random.nextDouble() - 0.5) * (random.nextInt(4) + 1),
+                    pos.getY() + 0.5 + (random.nextDouble() - 0.5) * (random.nextInt(4) + 1),
+                    pos.getZ() + 0.5 + (random.nextDouble() - 0.5) * (random.nextInt(4) + 1),
+                    0.0, 0.0, 0.0
+            );
+        }
+        if (random.nextInt(12) == 0) {
+            world.addParticle(
+                    ParticleTypes.SOUL_FIRE_FLAME,
+                    pos.getX() + random.nextDouble() * random.nextInt(3),
+                    pos.getY() + random.nextDouble() * random.nextInt(3),
+                    pos.getZ() + random.nextDouble() * random.nextInt(3),
+                    0.0, 0.0, 0.0
+            );
+        }
     }
 }
