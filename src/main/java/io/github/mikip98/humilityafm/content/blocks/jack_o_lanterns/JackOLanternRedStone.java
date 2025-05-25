0c73e41f60c0da78b324d404fe019aeb9b85f925
MikiP98 <pokora.mikolaj@gmail.com>
2025-05-25 23:43:54 +0200

    Made Redstone Jack o'Lantern turn off when powered

--- src/main/java/io/github/mikip98/humilityafm/content/blocks/jack_o_lanterns/JackOLanternRedStone.java
+++ src/main/java/io/github/mikip98/humilityafm/content/blocks/jack_o_lanterns/JackOLanternRedStone.java
@@ -1,9 +1,58 @@
 package io.github.mikip98.humilityafm.content.blocks.jack_o_lanterns;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class JackOLanternRedStone extends JackOLantern {
+    public static final BooleanProperty LIT = Properties.LIT;
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(LIT);
+    }
+
+
     public JackOLanternRedStone() {
         super(FabricBlockSettings.copyOf(defaultSettings).luminance(7));
+        setDefaultState(getDefaultState().with(LIT, true));
+    }
+
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(LIT, !ctx.getWorld().isReceivingRedstonePower(ctx.getBlockPos()));
+    }
+
+    @Override
+    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
+        if (!world.isClient) {
+            boolean isLit = state.get(LIT);
+            if (isLit == world.isReceivingRedstonePower(pos)) {
+                if (isLit) {
+                    world.setBlockState(pos, state.cycle(LIT), 2);
+                } else {
+                    world.scheduleBlockTick(pos, this, 4);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (!state.get(LIT) && !world.isReceivingRedstonePower(pos)) {
+            world.setBlockState(pos, state.cycle(LIT), 2);
+        }
     }
 }
