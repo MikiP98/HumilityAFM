64a560132ece2b19015da5d2b6ba68a6605d10e8
MikiP98 <pokora.mikolaj@gmail.com>
2025-05-25 15:06:34 +0200

    Improved coloured torch block settings

--- src/main/java/io/github/mikip98/humilityafm/generators/ColouredLightsGenerator.java
+++ src/main/java/io/github/mikip98/humilityafm/generators/ColouredLightsGenerator.java
@@ -1,9 +1,10 @@
 package io.github.mikip98.humilityafm.generators;
 
-import io.github.mikip98.humilityafm.content.blocks.light_strips.LightStripBlock;
+import io.github.mikip98.humilityafm.content.blocks.LightStripBlock;
 import io.github.mikip98.humilityafm.util.GenerationData;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.TorchBlock;
 import net.minecraft.particle.ParticleTypes;
 
@@ -16,9 +17,9 @@ public class ColouredLightsGenerator {
 
     public static void init() {
         for (byte i = 0; i < GenerationData.vanillaColorPallet.length; ++i) {
-            colouredTorchWeakVariants[i] = new TorchBlock(FabricBlockSettings.create().luminance(7), ParticleTypes.FLAME);
-            colouredTorchVariants[i] = new TorchBlock(FabricBlockSettings.create().luminance(11), ParticleTypes.FLAME);
-            colouredTorchStrongVariants[i] = new TorchBlock(FabricBlockSettings.create().luminance(15), ParticleTypes.FLAME);
+            colouredTorchWeakVariants[i] = new TorchBlock(FabricBlockSettings.copyOf(Blocks.TORCH).luminance(7), ParticleTypes.FLAME);
+            colouredTorchVariants[i] = new TorchBlock(FabricBlockSettings.copyOf(Blocks.TORCH).luminance(11), ParticleTypes.FLAME);
+            colouredTorchStrongVariants[i] = new TorchBlock(FabricBlockSettings.copyOf(Blocks.TORCH).luminance(15), ParticleTypes.FLAME);
             LightStripBlockVariants[i] = new LightStripBlock();
         }
     }
