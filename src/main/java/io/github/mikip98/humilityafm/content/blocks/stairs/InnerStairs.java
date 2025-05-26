958ea49a9ced189fe51ef5a20fa8d070c7a4e074
MikiP98 <pokora.mikolaj@gmail.com>
2025-05-26 23:17:49 +0200

    Fixed and improved Forced corner stairs voxel shapes

--- src/main/java/io/github/mikip98/humilityafm/content/blocks/stairs/InnerStairs.java
+++ src/main/java/io/github/mikip98/humilityafm/content/blocks/stairs/InnerStairs.java
@@ -12,27 +12,57 @@ import net.minecraft.world.BlockView;
 import java.util.Map;
 
 public class InnerStairs extends OuterStairs {
-    protected static final Map<Direction, VoxelShape> bottomVoxelShape = getVoxelShapeMapOfY(0.5f);
-    protected static final Map<Direction, VoxelShape> topVoxelShape = getVoxelShapeMapOfY(0f);
-    protected static Map<Direction, VoxelShape> getVoxelShapeMapOfY(double y) {
+    protected static final VoxelShape voxelShapeBottomNorth;
+    protected static final VoxelShape voxelShapeBottomSouth;
+    protected static final VoxelShape voxelShapeBottomEast;
+    protected static final VoxelShape voxelShapeBottomWest;
+
+    protected static final VoxelShape voxelShapeTopNorth;
+    protected static final VoxelShape voxelShapeTopSouth;
+    protected static final VoxelShape voxelShapeTopEast;
+    protected static final VoxelShape voxelShapeTopWest;
+
+    protected static Map<Direction, VoxelShape> getVoxelShapeMapOfY ( double y){
+        VoxelShape base = VoxelShapes.cuboid(
+                0f, 0.5f - y, 0f,
+                1f, 1f - y, 1f
+        );
         return Map.of(
                 Direction.NORTH, VoxelShapes.union(
-                        VoxelShapes.cuboid(0.5f, y, 0f, 1f, y+0.5f, 1f),
-                        VoxelShapes.cuboid(0f, y, 0.5f, 0.5f, y+0.5f, 1f)
+                        base,
+                        VoxelShapes.cuboid(0.5f, y, 0f, 1f, y + 0.5f, 1f),
+                        VoxelShapes.cuboid(0f, y, 0.5f, 0.5f, y + 0.5f, 1f)
                 ),  // original
                 Direction.SOUTH, VoxelShapes.union(
-                        VoxelShapes.cuboid(0f, y, 0f, 0.5f, y+0.5f, 1f),
-                        VoxelShapes.cuboid(0.5f, y, 0f, 1f, y+0.5f, 0.5f)
+                        base,
+                        VoxelShapes.cuboid(0f, y, 0f, 0.5f, y + 0.5f, 1f),
+                        VoxelShapes.cuboid(0.5f, y, 0f, 1f, y + 0.5f, 0.5f)
                 ),  // reverse original
                 Direction.EAST, VoxelShapes.union(
-                        VoxelShapes.cuboid(0f, y, 0.5f, 1f, y+0.5f, 1f),
-                        VoxelShapes.cuboid(0f, y, 0f, 0.5f, y+0.5f, 0.5f)
+                        base,
+                        VoxelShapes.cuboid(0f, y, 0.5f, 1f, y + 0.5f, 1f),
+                        VoxelShapes.cuboid(0f, y, 0f, 0.5f, y + 0.5f, 0.5f)
                 ),  // swap x <-> z + reverse
-                Direction.WEST,  VoxelShapes.union(
-                        VoxelShapes.cuboid(0f, y, 0f, 1f, y+0.5f, 0.5f),
-                        VoxelShapes.cuboid(0.5f, y, 0.5f, 1f, y+0.5f, 1f)
+                Direction.WEST, VoxelShapes.union(
+                        base,
+                        VoxelShapes.cuboid(0f, y, 0f, 1f, y + 0.5f, 0.5f),
+                        VoxelShapes.cuboid(0.5f, y, 0.5f, 1f, y + 0.5f, 1f)
                 )   // swap x <-> z
         );
+    }
+    static {
+        final Map<Direction, VoxelShape> bottomVoxelShape = getVoxelShapeMapOfY(0.5f);
+        final Map<Direction, VoxelShape> topVoxelShape = getVoxelShapeMapOfY(0f);
+
+        voxelShapeBottomNorth = bottomVoxelShape.get(Direction.NORTH);
+        voxelShapeBottomSouth = bottomVoxelShape.get(Direction.SOUTH);
+        voxelShapeBottomEast = bottomVoxelShape.get(Direction.EAST);
+        voxelShapeBottomWest = bottomVoxelShape.get(Direction.WEST);
+
+        voxelShapeTopNorth = topVoxelShape.get(Direction.NORTH);
+        voxelShapeTopSouth = topVoxelShape.get(Direction.SOUTH);
+        voxelShapeTopEast = topVoxelShape.get(Direction.EAST);
+        voxelShapeTopWest = topVoxelShape.get(Direction.WEST);
     }
 
     public InnerStairs(Settings settings) {
@@ -42,8 +72,37 @@ public class InnerStairs extends OuterStairs {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         Direction dir = state.get(FACING);
-        return state.get(Properties.BLOCK_HALF) == BlockHalf.TOP
-                ? topVoxelShape.get(dir)
-                : bottomVoxelShape.get(dir);
+        if (state.get(Properties.BLOCK_HALF) == BlockHalf.BOTTOM) {
+            switch (dir) {
+                case NORTH -> {
+                    return voxelShapeBottomNorth;
+                }
+                case SOUTH -> {
+                    return voxelShapeBottomSouth;
+                }
+                case EAST -> {
+                    return voxelShapeBottomEast;
+                }
+                case WEST -> {
+                    return voxelShapeBottomWest;
+                }
+            }
+        } else {
+            switch (dir) {
+                case NORTH -> {
+                    return voxelShapeTopNorth;
+                }
+                case SOUTH -> {
+                    return voxelShapeTopSouth;
+                }
+                case EAST -> {
+                    return voxelShapeTopEast;
+                }
+                case WEST -> {
+                    return voxelShapeTopWest;
+                }
+            }
+        }
+        return VoxelShapes.fullCube();  // Fallback, should not happen
     }
 }
--- src/main/java/io/github/mikip98/humilityafm/content/blocks/stairs/OuterStairs.java
+++ src/main/java/io/github/mikip98/humilityafm/content/blocks/stairs/OuterStairs.java
@@ -17,21 +17,41 @@ import net.minecraft.world.BlockView;
 import java.util.Map;
 
 public class OuterStairs extends HorizontalFacingBlock implements Waterloggable {
-    protected static final Map<Direction, VoxelShape> bottomVoxelShape = getVoxelShapeMapOfY(
-            VoxelShapes.cuboid(0f, 0f, 0f, 1f, 0.5f, 1.0f),
-            0.5f
-    );
-    protected static final Map<Direction, VoxelShape> topVoxelShape = getVoxelShapeMapOfY(
-            VoxelShapes.cuboid(0f, 0.5f, 0f, 1f, 1f, 1.0f),
-            0f
-    );
-    protected static Map<Direction, VoxelShape> getVoxelShapeMapOfY(VoxelShape base, double y) {
+    protected static final VoxelShape voxelShapeBottomNorth;
+    protected static final VoxelShape voxelShapeBottomSouth;
+    protected static final VoxelShape voxelShapeBottomEast;
+    protected static final VoxelShape voxelShapeBottomWest;
+
+    protected static final VoxelShape voxelShapeTopNorth;
+    protected static final VoxelShape voxelShapeTopSouth;
+    protected static final VoxelShape voxelShapeTopEast;
+    protected static final VoxelShape voxelShapeTopWest;
+
+    protected static Map<Direction, VoxelShape> getVoxelShapeMapOfY(double y) {
+        VoxelShape base = VoxelShapes.cuboid(
+                0f, 0.5f - y, 0f,
+                1f, 1f - y, 1f
+        );
         return Map.of(
                 Direction.NORTH, VoxelShapes.union(base, VoxelShapes.cuboid(0.5f, y, 0.5f, 1.0f, y+0.5f, 1.0f)),  // original
                 Direction.SOUTH, VoxelShapes.union(base, VoxelShapes.cuboid(0.0f, y, 0.0f, 0.5f, y+0.5f, 0.5f)),  // reverse original
                 Direction.EAST,  VoxelShapes.union(base, VoxelShapes.cuboid(0.0f, y, 0.5f, 0.5f, y+0.5f, 1.0f)),  // swap x <-> z + reverse
                 Direction.WEST,  VoxelShapes.union(base, VoxelShapes.cuboid(0.5f, y, 0.0f, 1.0f, y+0.5f, 0.5f))   // swap x <-> z
         );
+    }
+    static {
+        final Map<Direction, VoxelShape> bottomVoxelShape = getVoxelShapeMapOfY(0.5f);
+        final Map<Direction, VoxelShape> topVoxelShape = getVoxelShapeMapOfY(0f);
+
+        voxelShapeBottomNorth = bottomVoxelShape.get(Direction.NORTH);
+        voxelShapeBottomSouth = bottomVoxelShape.get(Direction.SOUTH);
+        voxelShapeBottomEast = bottomVoxelShape.get(Direction.EAST);
+        voxelShapeBottomWest = bottomVoxelShape.get(Direction.WEST);
+
+        voxelShapeTopNorth = topVoxelShape.get(Direction.NORTH);
+        voxelShapeTopSouth = topVoxelShape.get(Direction.SOUTH);
+        voxelShapeTopEast = topVoxelShape.get(Direction.EAST);
+        voxelShapeTopWest = topVoxelShape.get(Direction.WEST);
     }
 
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
@@ -69,8 +89,29 @@ public class OuterStairs extends HorizontalFacingBlock implements Waterloggable 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         Direction dir = state.get(FACING);
-        return state.get(Properties.BLOCK_HALF) == BlockHalf.TOP
-                ? topVoxelShape.get(dir)
-                : bottomVoxelShape.get(dir);
+        if (state.get(Properties.BLOCK_HALF) == BlockHalf.BOTTOM) {
+            switch (dir) {
+                case NORTH:
+                    return voxelShapeBottomNorth;
+                case SOUTH:
+                    return voxelShapeBottomSouth;
+                case EAST:
+                    return voxelShapeBottomEast;
+                case WEST:
+                    return voxelShapeBottomWest;
+            }
+        } else {
+            switch (dir) {
+                case NORTH:
+                    return voxelShapeTopNorth;
+                case SOUTH:
+                    return voxelShapeTopSouth;
+                case EAST:
+                    return voxelShapeTopEast;
+                case WEST:
+                    return voxelShapeTopWest;
+            }
+        }
+        return VoxelShapes.fullCube();  // Fallback, should not happen
     }
 }
