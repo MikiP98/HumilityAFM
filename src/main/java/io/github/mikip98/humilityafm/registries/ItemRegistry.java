caa5a899a4db1027e07c89d078c3f8cc580c5d9a
MikiP98 <pokora.mikolaj@gmail.com>
2025-07-08 00:25:32 +0200

    Updated ItemRegistry to 1.21.4

--- src/main/java/io/github/mikip98/humilityafm/registries/ItemRegistry.java
+++ src/main/java/io/github/mikip98/humilityafm/registries/ItemRegistry.java
@@ -5,18 +5,20 @@ import io.github.mikip98.humilityafm.content.items.DoubleVerticallyAttachableBlo
 import io.github.mikip98.humilityafm.util.GenerationData;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import static io.github.mikip98.humilityafm.HumilityAFM.getId;
 import static io.github.mikip98.humilityafm.registries.ItemGroupRegistry.putIntoItemGroup;
 
 public class ItemRegistry {
-    public static final Item[] GLOWING_POWDER_VARIANTS = Arrays.stream(GenerationData.vanillaColorPallet).map(color -> register("glowing_powder_" + color)).toArray(Item[]::new);
+    public static final Item[] GLOWING_POWDER_VARIANTS = registerArray("glowing_powder_", GenerationData.vanillaColorPallet, Item::new);
 
     public static Item[] CABINET_ITEM_VARIANTS;
     public static Item[] ILLUMINATED_CABINET_ITEM_VARIANTS;
@@ -26,12 +28,14 @@ public class ItemRegistry {
 
 
     public static final Item CABINET_ITEM = register(
-            new DoubleVerticallyAttachableBlockItem(BlockRegistry.FLOOR_CABINET_BLOCK, BlockRegistry.CABINET_BLOCK, new Item.Settings()),
-            "cabinet_block"
+            "cabinet_block",
+            (settings) -> new DoubleVerticallyAttachableBlockItem(BlockRegistry.FLOOR_CABINET_BLOCK, BlockRegistry.CABINET_BLOCK, settings),
+            new Item.Settings()
     );
     public static final Item ILLUMINATED_CABINET_ITEM = register(
-            new DoubleVerticallyAttachableBlockItem(BlockRegistry.FLOOR_ILLUMINATED_CABINET_BLOCK, BlockRegistry.ILLUMINATED_CABINET_BLOCK, new Item.Settings()),
-            "illuminated_cabinet_block"
+            "illuminated_cabinet_block",
+            (settings) -> new DoubleVerticallyAttachableBlockItem(BlockRegistry.FLOOR_ILLUMINATED_CABINET_BLOCK, BlockRegistry.ILLUMINATED_CABINET_BLOCK, settings),
+            new Item.Settings()
     );
 
 
@@ -48,19 +52,14 @@ public class ItemRegistry {
     }
 
 
-    protected static void registerArray(Item[] items, String[] names, String prefix) {
-        for (int i = 0; i < items.length; i++) {
-            register(items[i], prefix + names[i]);
-        }
+    public static Item[] registerArray(String prefix, String[] names, Function<Item.Settings, Item> factory) {
+        return Arrays.stream(names).map(name -> register(prefix + name, factory)).toArray(Item[]::new);
     }
-    public static Item register(Item item, String name) {
-        return register(name, item);
+    public static Item register(String name, Function<Item.Settings, Item> factory) {
+        return register(name, factory, new Item.Settings());
     }
-    public static Item register(String name) {
-        return register(name, new Item(new Item.Settings()));
-    }
-    public static Item register(String name, Item item) {
-        Registry.register(Registries.ITEM, getId(name), item);
-        return item;
+    public static Item register(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, getId(name));
+        return Items.register(registryKey, factory, settings);
     }
 }
