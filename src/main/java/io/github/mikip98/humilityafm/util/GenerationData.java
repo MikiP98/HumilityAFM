0763e50b9d057500aeeebc8923698f3f3e3855e9
MikiP98 <pokora.mikolaj@gmail.com>
2025-07-08 15:30:10 +0200

    Added Pale Oak wood type

--- src/main/java/io/github/mikip98/humilityafm/util/GenerationData.java
+++ src/main/java/io/github/mikip98/humilityafm/util/GenerationData.java
@@ -20,7 +20,7 @@ public class GenerationData {
     /***
      * All vanilla wood types
      */
-    public static final String[] vanillaWoodTypes = {"oak", "spruce", "birch", "jungle", "acacia", "dark_oak", "mangrove", "cherry", "bamboo", "crimson", "warped"};
+    public static final String[] vanillaWoodTypes = {"oak", "spruce", "birch", "jungle", "acacia", "dark_oak", "mangrove", "cherry", "bamboo", "pale_oak", "crimson", "warped"};
     public static final float vanillaWoodHardness = Blocks.OAK_PLANKS.getHardness();  // 2.0f;
     public static final float vanillaWoodResistance = Blocks.OAK_PLANKS.getBlastResistance();  // 3.0f;
     // TODO: Split nether and overworld wood types, as nether wood does not burn
