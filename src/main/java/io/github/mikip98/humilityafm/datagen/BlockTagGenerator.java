4f4a921c7376d18af6c61dfbb869e87b253c7578
MikiP98 <pokora.mikolaj@gmail.com>
2025-07-09 12:39:58 +0200

    Updated BlockTagGenerator to 1.21.6

--- src/main/java/io/github/mikip98/humilityafm/datagen/BlockTagGenerator.java
+++ src/main/java/io/github/mikip98/humilityafm/datagen/BlockTagGenerator.java
@@ -5,19 +5,21 @@ import io.github.mikip98.humilityafm.generators.CabinetBlockGenerator;
 import io.github.mikip98.humilityafm.generators.CandlestickGenerator;
 import io.github.mikip98.humilityafm.generators.TerracottaTilesGenerator;
 import io.github.mikip98.humilityafm.generators.WoodenMosaicGenerator;
+import io.github.mikip98.humilityafm.mixin.BlockSettingsAccessor;
 import io.github.mikip98.humilityafm.registries.BlockRegistry;
 import io.github.mikip98.humilityafm.util.GenerationData;
 import io.github.mikip98.humilityafm.util.data_types.Pair;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.block.Block;
+import net.minecraft.data.tag.ProvidedTagBuilder;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 
-import static io.github.mikip98.humilityafm.HumilityAFM.getId;
-import static io.github.mikip98.humilityafm.HumilityAFM.getIds;
+import static io.github.mikip98.humilityafm.registries.BlockRegistry.keyOfBlock;
 
 public class BlockTagGenerator extends FabricTagProvider.BlockTagProvider {
     public BlockTagGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
@@ -28,27 +30,29 @@ public class BlockTagGenerator extends FabricTagProvider.BlockTagProvider {
     protected void configure(RegistryWrapper.WrapperLookup arg) {
         // ------------ Custom Tags ------------
         // Cabinet Block Tags
-        getOrCreateTagBuilder(ModBlockTags.CABINET_BLOCKS)
+        // TODO: I see that 'valueLookupBuilder' takes in Blocks and 'builder' takes in RegistryKey<Block>...
+        //  Well... Now is a bit to late to learn this fact... I'm keeping this as it is (as long as it runs)
+        valueLookupBuilder(ModBlockTags.CABINET_BLOCKS)
                 .add(CabinetBlockGenerator.cabinetBlockVariants)
                 .add(BlockRegistry.CABINET_BLOCK);  // Manual testing block
-        getOrCreateTagBuilder(ModBlockTags.ILLUMINATED_CABINET_BLOCKS)
-                .add(CabinetBlockGenerator.illuminatedCabinetBlockVariants)
-                .add(BlockRegistry.ILLUMINATED_CABINET_BLOCK);  // Manual testing block
-        getOrCreateTagBuilder(ModBlockTags.CABINET_BLOCKS)
-                .add(CabinetBlockGenerator.floorCabinetBlockVariants)
-                .add(BlockRegistry.FLOOR_CABINET_BLOCK);  // Manual testing block
-        getOrCreateTagBuilder(ModBlockTags.ILLUMINATED_CABINET_BLOCKS)
-                .add(CabinetBlockGenerator.floorIlluminatedCabinetBlockVariants)
-                .add(BlockRegistry.FLOOR_ILLUMINATED_CABINET_BLOCK);  // Manual testing block
+        builder(ModBlockTags.ILLUMINATED_CABINET_BLOCKS)
+                .add(toRegistryKey(CabinetBlockGenerator.illuminatedCabinetBlockVariants))
+                .add(toRegistryKey(BlockRegistry.ILLUMINATED_CABINET_BLOCK));  // Manual testing block
+        builder(ModBlockTags.CABINET_BLOCKS)
+                .add(toRegistryKey(CabinetBlockGenerator.floorCabinetBlockVariants))
+                .add(toRegistryKey(BlockRegistry.FLOOR_CABINET_BLOCK));  // Manual testing block
+        builder(ModBlockTags.ILLUMINATED_CABINET_BLOCKS)
+                .add(toRegistryKey(CabinetBlockGenerator.floorIlluminatedCabinetBlockVariants))
+                .add(toRegistryKey(BlockRegistry.FLOOR_ILLUMINATED_CABINET_BLOCK));  // Manual testing block
 
         // Wooden Mosaic Block Tag
-        getOrCreateTagBuilder(ModBlockTags.WOODEN_MOSAIC_BLOCKS)
-                .add(WoodenMosaicGenerator.woodenMosaicVariants)
-                .add(BlockRegistry.WOODEN_MOSAIC);  // Manual testing block
+        builder(ModBlockTags.WOODEN_MOSAIC_BLOCKS)
+                .add(toRegistryKey(WoodenMosaicGenerator.woodenMosaicVariants))
+                .add(toRegistryKey(BlockRegistry.WOODEN_MOSAIC));  // Manual testing block
 
         // Terracotta Tiles Block Tag
-        getOrCreateTagBuilder(ModBlockTags.TERRACOTTA_TILES_BLOCKS)
-                .add(TerracottaTilesGenerator.terracottaTilesVariants);
+        builder(ModBlockTags.TERRACOTTA_TILES_BLOCKS)
+                .add(toRegistryKey(TerracottaTilesGenerator.terracottaTilesVariants));
 
         // Inner & Outer Stairs Block Tags
         String[] woodenVariants = GenerationData.vanillaWoodTypes;
@@ -56,38 +60,38 @@ public class BlockTagGenerator extends FabricTagProvider.BlockTagProvider {
                 .flatMap(Arrays::stream).toArray(String[]::new);
 
         // Inner Stairs
-        getOrCreateTagBuilder(ModBlockTags.WOODEN_INNER_STAIRS)
-                .add(getIds(Arrays.stream(woodenVariants).map(s -> "inner_stairs_" + s)))
-                .add(BlockRegistry.INNER_STAIRS);  // Manual testing block
-        getOrCreateTagBuilder(ModBlockTags.STONE_INNER_STAIRS)
-                .add(getIds(Arrays.stream(stonyVariants).map(s -> "inner_stairs_" + s)));
+        builder(ModBlockTags.WOODEN_INNER_STAIRS)
+                .add(Arrays.stream(woodenVariants).map(s -> keyOfBlock("inner_stairs_" + s)))
+                .add(toRegistryKey(BlockRegistry.INNER_STAIRS));  // Manual testing block
+        builder(ModBlockTags.STONE_INNER_STAIRS)
+                .add(Arrays.stream(stonyVariants).map(s -> keyOfBlock("inner_stairs_" + s)));
         // Outer Stairs
-        getOrCreateTagBuilder(ModBlockTags.WOODEN_OUTER_STAIRS)
-                .add(getIds(Arrays.stream(woodenVariants).map(s -> "outer_stairs_" + s)))
-                .add(BlockRegistry.OUTER_STAIRS);  // Manual testing block
-        getOrCreateTagBuilder(ModBlockTags.STONE_OUTER_STAIRS)
-                .add(getIds(Arrays.stream(stonyVariants).map(s -> "outer_stairs_" + s)));
+        builder(ModBlockTags.WOODEN_OUTER_STAIRS)
+                .add(Arrays.stream(woodenVariants).map(s -> keyOfBlock("outer_stairs_" + s)))
+                .add(toRegistryKey(BlockRegistry.OUTER_STAIRS));  // Manual testing block
+        builder(ModBlockTags.STONE_OUTER_STAIRS)
+                .add(Arrays.stream(stonyVariants).map(s -> keyOfBlock("outer_stairs_" + s)));
 
         // Jack o'Lanterns
-        getOrCreateTagBuilder(ModBlockTags.JACK_O_LANTERNS)
-                .add(BlockRegistry.JACK_O_LANTERN_SOUL)
-                .add(BlockRegistry.JACK_O_LANTERN_REDSTONE);
-        getOrCreateTagBuilder(ModBlockTags.COLOURED_JACK_O_LANTERNS)
-                .add(BlockRegistry.COLOURED_WEAK_JACK_O_LANTERNS)
-                .add(BlockRegistry.COLOURED_WEAK_JACK_O_LANTERNS)
-                .add(BlockRegistry.COLOURED_STRONG_JACK_O_LANTERNS);
+        builder(ModBlockTags.JACK_O_LANTERNS)
+                .add(toRegistryKey(BlockRegistry.JACK_O_LANTERN_SOUL))
+                .add(toRegistryKey(BlockRegistry.JACK_O_LANTERN_REDSTONE));
+        builder(ModBlockTags.COLOURED_JACK_O_LANTERNS)
+                .add(toRegistryKey(BlockRegistry.COLOURED_WEAK_JACK_O_LANTERNS))
+                .add(toRegistryKey(BlockRegistry.COLOURED_WEAK_JACK_O_LANTERNS))
+                .add(toRegistryKey(BlockRegistry.COLOURED_STRONG_JACK_O_LANTERNS));
 
         // Candlesticks
-        FabricTagProvider<Block>.FabricTagBuilder tag = getOrCreateTagBuilder(ModBlockTags.CANDLESTICKS)
-                .add(CandlestickGenerator.candlestickClassicStandingVariants)
-                .add(CandlestickGenerator.candlestickClassicWallVariants);
-        CandlestickGenerator.candlestickRustableStandingVariants.forEach(tag::add);
-        CandlestickGenerator.candlestickRustableWallVariants.forEach(tag::add);
+        ProvidedTagBuilder<RegistryKey<Block>, Block> tag = builder(ModBlockTags.CANDLESTICKS)
+                .add(toRegistryKey(CandlestickGenerator.candlestickClassicStandingVariants))
+                .add(toRegistryKey(CandlestickGenerator.candlestickClassicWallVariants));
+        CandlestickGenerator.candlestickRustableStandingVariants.forEach((block) -> tag.add(toRegistryKey(block)));
+        CandlestickGenerator.candlestickRustableWallVariants.forEach((block) -> tag.add(toRegistryKey(block)));
 
 
         // ------------ Vanilla Tags ------------
         // Axe Mineable
-        getOrCreateTagBuilder(ModBlockTags.AXE_MINEABLE)
+        builder(ModBlockTags.AXE_MINEABLE)
                 // Cabinets
                 .addTag(ModBlockTags.CABINET_BLOCKS)
                 .addTag(ModBlockTags.ILLUMINATED_CABINET_BLOCKS)
@@ -102,7 +106,7 @@ public class BlockTagGenerator extends FabricTagProvider.BlockTagProvider {
                 .addTag(ModBlockTags.JACK_O_LANTERNS)
                 .addOptionalTag(ModBlockTags.COLOURED_JACK_O_LANTERNS);
         // Pickaxe Mineable
-        getOrCreateTagBuilder(ModBlockTags.PICKAXE_MINEABLE)
+        builder(ModBlockTags.PICKAXE_MINEABLE)
                 // Terracotta Tiles
                 .addTag(ModBlockTags.TERRACOTTA_TILES_BLOCKS)
                 // Inner & Outer Stairs
@@ -110,5 +114,13 @@ public class BlockTagGenerator extends FabricTagProvider.BlockTagProvider {
                 .addTag(ModBlockTags.STONE_OUTER_STAIRS)
                 // Candlesticks
                 .addOptionalTag(ModBlockTags.CANDLESTICKS);
+    }
+
+    @SuppressWarnings("unchecked")
+    protected static RegistryKey<Block>[] toRegistryKey(Block[] blocks) {
+        return (RegistryKey<Block>[]) Arrays.stream(blocks).map(BlockTagGenerator::toRegistryKey).toArray(RegistryKey[]::new);
+    }
+    protected static RegistryKey<Block> toRegistryKey(Block block) {
+        return ((BlockSettingsAccessor) block.getSettings()).getRegistryKey();
     }
 }
