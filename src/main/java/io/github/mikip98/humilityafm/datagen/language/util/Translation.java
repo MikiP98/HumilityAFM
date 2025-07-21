7ed9cc3b1b27460763387c57037efa64f39ac39d
MikiP98 <pokora.mikolaj@gmail.com>
2025-07-21 11:23:12 +0200

    Improved Translation class

--- src/main/java/io/github/mikip98/humilityafm/datagen/language/util/Translation.java
+++ src/main/java/io/github/mikip98/humilityafm/datagen/language/util/Translation.java
@@ -1,5 +1,8 @@
 package io.github.mikip98.humilityafm.datagen.language.util;
 
+import lombok.Getter;
+
+@Getter
 public class Translation {
     private String string;
 
@@ -10,8 +13,5 @@ public class Translation {
     public Translation translate(String input, String output) {
         this.string = this.string.replace(input, output).replace(input.toLowerCase(), output.toLowerCase());
         return this;
-    }
-    public String getString() {
-        return string;
     }
 }
