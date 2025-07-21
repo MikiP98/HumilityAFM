955490b09661af110ccef2534999ad19b2c0be10
MikiP98 <pokora.mikolaj@gmail.com>
2025-07-21 11:20:37 +0200

    Polish translation update

--- src/main/java/io/github/mikip98/humilityafm/datagen/language/PolishLangProvider.java
+++ src/main/java/io/github/mikip98/humilityafm/datagen/language/PolishLangProvider.java
@@ -1,5 +1,6 @@
 package io.github.mikip98.humilityafm.datagen.language;
 
+import io.github.mikip98.humilityafm.datagen.language.util.Translation;
 import io.github.mikip98.humilityafm.datagen.language.util.TranslationCategory;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
@@ -44,39 +45,29 @@ public class PolishLangProvider extends FabricLanguageProvider {
         blockTranslations = new Hashtable<>(blockTranslations); // Without this line, fabric throws 'java.util.ConcurrentModificationException' exception
         for (Map.Entry<String, String> entry : blockTranslations.entrySet()) {
             String key = entry.getKey();
-            String value = entry.getValue();
+            Translation value = new Translation(entry.getValue());
             blockTranslations.put(
                     key,
                     value
                             // Test blocks
-                            .replace("Test", "Testowy")
-                            .replace("test", "testowy")
+                            .translate("Test", "Testowy")
                             // Cabinet blocks
-                            .replace("Illuminated Cabinet", "Podświetlana Gablota")
-                            .replace("illuminated cabinet", "podświetlana gablota")
-                            .replace("Cabinet", "Gablota")
-                            .replace("cabinet", "gablota")
+                            .translate("Illuminated Cabinet", "Podświetlana Gablota")
+                            .translate("Cabinet", "Gablota")
                             // Wooden Mosaics
-                            .replace("Wooden Mosaic", "Drewniana Mozaika")
-                            .replace("wooden mosaic", "drewniana mozaika")
+                            .translate("Wooden Mosaic", "Drewniana Mozaika")
                             // Terracotta Tiles
-                            .replace("Terracotta Tiles", "Płytki z Terakoty")
-                            .replace("terracotta tiles", "płytki z terakoty")
+                            .translate("Terracotta Tiles", "Płytki z Terakoty")
                             // Inner and outer stairs
-                            .replace("Inner Stairs", "Schody Wewnętrzne")
-                            .replace("inner stairs", "schody wewnętrzne")
-                            .replace("Outer Stairs", "Schody Zewnętrzne")
-                            .replace("outer stairs", "schody zewnętrzne")
+                            .translate("Inner Stairs", "Schody Wewnętrzne")
+                            .translate("Outer Stairs", "Schody Zewnętrzne")
                             // LEDs
-                            .replace("LED Powder", "Proszek LED")
-                            .replace("LED powder", "proszek LED")
+                            .translate("Glowing Powder", "Świecący Proszek")
                             // Candlesticks
-                            .replace("Candlestick", "Świecznik")
-                            .replace("candlestick", "świecznik")
-                            .replace("With", "Z")
-                            .replace("with", "z")
-                            .replace("Candle", "Świeczką")
-                            .replace("candle", "świeczką")
+                            .translate("Candlestick", "Świecznik")
+                            .translate("With", "Z")
+                            .translate("Candle", "Świeczką")
+                            .getString()
                     // TODO: Translate wood types, colours and 'block'
                     //  For cabinet blocks e.g. "Oak" -> "Dębowa", "Birch" -> "Brzozowa", etc.
                     //  For wooden mosaics the first wood type should end with 'owo' instead of 'owa'
