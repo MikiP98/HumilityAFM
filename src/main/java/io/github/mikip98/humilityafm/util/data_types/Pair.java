269bd93001dd3f59d8bc99c42f784a1b3f048029
MikiP98 <pokora.mikolaj@gmail.com>
2025-08-20 14:47:42 +0200

    Added another way of accessing the fields

--- src/main/java/io/github/mikip98/humilityafm/util/data_types/Pair.java
+++ src/main/java/io/github/mikip98/humilityafm/util/data_types/Pair.java
@@ -4,4 +4,6 @@ public record Pair<A, B>(A first, B second) {
     public static <A, B> Pair<A, B> of(A first, B second) {
         return new Pair<>(first, second);
     }
+    public A left() { return first; }
+    public B right() { return second; }
 }
