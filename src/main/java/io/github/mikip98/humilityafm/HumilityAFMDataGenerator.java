b97f2d1186263788a5dc952be4e140e880e79661
MikiP98 <pokora.mikolaj@gmail.com>
2025-08-27 22:35:33 +0200

    Added small datagenMode setting check during datagen

--- src/main/java/io/github/mikip98/humilityafm/HumilityAFMDataGenerator.java
+++ src/main/java/io/github/mikip98/humilityafm/HumilityAFMDataGenerator.java
@@ -1,5 +1,6 @@
 package io.github.mikip98.humilityafm;
 
+import io.github.mikip98.humilityafm.config.ModConfig;
 import io.github.mikip98.humilityafm.datagen.BlockLootTableGenerator;
 import io.github.mikip98.humilityafm.datagen.BlockTagGenerator;
 import io.github.mikip98.humilityafm.datagen.ModelGenerator;
@@ -13,6 +14,9 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 public class HumilityAFMDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		if (!ModConfig.datagenMode)
+			throw new IllegalStateException("Data generation mode is disabled! Please enable it in the config file to run Fabric Datagen for this project.");
+
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
 		pack.addProvider(BlockTagGenerator::new);
