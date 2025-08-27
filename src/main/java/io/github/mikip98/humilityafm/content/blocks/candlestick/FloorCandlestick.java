f4d9f236d68e3147e075df81117de02d67cc0a75
MikiP98 <pokora.mikolaj@gmail.com>
2025-08-27 21:58:48 +0200

    Forgot to include in the Candlestick commits :/

--- src/main/java/io/github/mikip98/humilityafm/content/blocks/candlestick/FloorCandlestick.java
+++ src/main/java/io/github/mikip98/humilityafm/content/blocks/candlestick/FloorCandlestick.java
@@ -1,88 +1,97 @@
 package io.github.mikip98.humilityafm.content.blocks.candlestick;
 
 import io.github.mikip98.humilityafm.content.ModProperties;
+import io.github.mikip98.humilityafm.content.blocks.candlestick.logic.SimpleCandlestickLogic;
 import io.github.mikip98.humilityafm.util.data_types.CandleColor;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.Waterloggable;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.particle.ParticleTypes;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.FluidTags;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-public class FloorCandlestick extends Candlestick {
+public class FloorCandlestick extends Block implements SimpleCandlestickLogic, Waterloggable {
     protected static final VoxelShape voxelShape = Block.createCuboidShape(6, 0, 6, 10, 6, 10);
     protected static final VoxelShape voxelShapeCandle = Block.createCuboidShape(6, 0, 6, 10, 10, 10);
+
+    protected static final EnumProperty<CandleColor> CANDLE_COLOR = ModProperties.CANDLE_COLOR;
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(Properties.WATERLOGGED);
-        builder.add(ModProperties.CANDLE);
         builder.add(CANDLE_COLOR);
         builder.add(Properties.LIT);
     }
 
     public FloorCandlestick() {
-        this(defaultSettings);
+        this(Candlestick.defaultSettings);
     }
     public FloorCandlestick(Settings settings) {
-        super(settings, false);
-        setDefaultState(getStateManager().getDefaultState()
+        super(settings);
+        setDefaultState(getDefaultState()
                 .with(Properties.WATERLOGGED, false)
-                .with(ModProperties.CANDLE, false)
-                .with(CANDLE_COLOR, CandleColor.PLAIN)
+                .with(CANDLE_COLOR, CandleColor.NONE)
                 .with(Properties.LIT, false)
         );
     }
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState()
+        return getDefaultState()
                 .with(Properties.WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).isIn(FluidTags.WATER));
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (onUseLogic(state, world, pos, player, hand, hit)) return ActionResult.SUCCESS;
+        return super.onUse(state, world, pos, player, hand, hit);
     }
 
     @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
-        // Check if the block is lit (property LIT is true)
+        // If the candle is lit, display flame and smoke particles + play sound
         if (state.get(Properties.LIT)) {
-            double x = pos.getX() + 0.5;
-            double y = pos.getY() + 0.7;
-            double z = pos.getZ() + 0.5;
-            if (random.nextInt(1) == 0) {
-                float velocityPlaneMultiplayer = 0.001953125f;
-                double velocityY = random.nextDouble() * 0.001953125;
-                double velocityX = (random.nextDouble() - 0.5) * velocityPlaneMultiplayer;
-                double velocityZ = (random.nextDouble() - 0.5) * velocityPlaneMultiplayer;
-                world.addParticle(ParticleTypes.SMALL_FLAME, x, y, z, velocityX, velocityY, velocityZ);
-
-                if (random.nextInt(4) == 0) {
-                    velocityPlaneMultiplayer = 0.00390625f;
-                    velocityY = random.nextDouble() * 0.00390625;
-                    velocityX = (random.nextDouble() - 0.5) * velocityPlaneMultiplayer;
-                    velocityZ = (random.nextDouble() - 0.5) * velocityPlaneMultiplayer;
-                    world.addParticle(ParticleTypes.SMOKE, x, y, z, velocityX, velocityY, velocityZ);
-                }
-            }
-            if (random.nextInt(4) == 0) {
-                world.playSound(x, y, z, SoundEvents.BLOCK_CANDLE_AMBIENT, SoundCategory.BLOCKS, 1.0f, 1.0f, true);
-            }
+            final double candleWickX = pos.getX() + 0.5;
+            final double candleWickY = pos.getY() + 0.667;
+            final double candleWickZ = pos.getZ() + 0.5;
+            performRandomDisplayTick(world, candleWickX, candleWickY, candleWickZ, random);
         }
-        super.randomDisplayTick(state, world, pos, random, false);
+        super.randomDisplayTick(state, world, pos, random);
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-        if (state.get(ModProperties.CANDLE)) {
-            return voxelShapeCandle;
-        }
-        return voxelShape;
+        return (state.get(CANDLE_COLOR) != CandleColor.NONE) ? voxelShapeCandle : voxelShape;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (state.getBlock() != newState.getBlock() && state.get(CANDLE_COLOR) != CandleColor.NONE)
+            Block.dropStack(world, pos, new ItemStack(state.get(CANDLE_COLOR).asCandle()));
+        super.onStateReplaced(state, world, pos, newState, moved);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(Properties.WATERLOGGED) ? Fluids.WATER.getStill(false) : Fluids.EMPTY.getDefaultState();
     }
 }
