91cac5478877a297c148c2f7767310ec2dc808d4
MikiP98 <pokora.mikolaj@gmail.com>
2025-08-27 21:56:28 +0200

    Created universal block material

---
+++ src/main/java/io/github/mikip98/humilityafm/util/generation_data/material_management/data_types/BlockStrength.java
@@ -0,0 +1,6 @@
+package io.github.mikip98.humilityafm.util.generation_data.material_management.data_types;
+
+public record BlockStrength(float hardness, float resistance) {
+    public static BlockStrength of(float hardness, float resistance) { return new BlockStrength(hardness, resistance); }
+    public static BlockStrength of(float hardness) { return new BlockStrength(hardness, hardness); }
+}
