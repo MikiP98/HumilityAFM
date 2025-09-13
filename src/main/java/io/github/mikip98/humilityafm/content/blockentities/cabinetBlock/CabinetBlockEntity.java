0799bbb3343c90d81ac56c5de2bf79ee347bfc14
MikiP98 <pokora.mikolaj@gmail.com>
2025-09-13 21:29:25 +0200

    Small code clean-up

--- src/main/java/io/github/mikip98/humilityafm/content/blockentities/cabinetBlock/CabinetBlockEntity.java
+++ src/main/java/io/github/mikip98/humilityafm/content/blockentities/cabinetBlock/CabinetBlockEntity.java
@@ -17,60 +17,46 @@ import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.Nullable;
 
 public class CabinetBlockEntity extends BlockEntity implements ImplementedInventory, SidedInventory {
-
     private final DefaultedList<ItemStack> items = DefaultedList.ofSize(1, ItemStack.EMPTY);
-
 
     public CabinetBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
-
     public CabinetBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntityRegistry.CABINET_BLOCK_ENTITY, pos, state);
     }
 
-
+    // Required by ImplementedInventory
     @Override
     public DefaultedList<ItemStack> getItems() {
         return this.items;
     }
 
+    // Required for the stored item to show up
     @Override
     public void readNbt(NbtCompound nbt) {
         super.readNbt(nbt);
         Inventories.readNbt(nbt, items);
     }
-
     @Override
     public void writeNbt(NbtCompound nbt) {
         Inventories.writeNbt(nbt, items);
         super.writeNbt(nbt);
     }
-
-    @Nullable
     @Override
-    public Packet<ClientPlayPacketListener> toUpdatePacket() {
+    public @Nullable Packet<ClientPlayPacketListener> toUpdatePacket() {
         return BlockEntityUpdateS2CPacket.create(this);
     }
-
     @Override
     public NbtCompound toInitialChunkDataNbt() {
         return createNbt();
     }
 
-
+    // Required to block hoppers from working
     @Override
-    public int[] getAvailableSlots(Direction var1) {
-        return new int[0];
-    }
-
+    public int[] getAvailableSlots(Direction var1) { return new int[0]; }
     @Override
-    public boolean canInsert(int slot, ItemStack stack, Direction direction) {
-        return false;
-    }
-
+    public boolean canInsert(int slot, ItemStack stack, Direction direction) { return false; }
     @Override
-    public boolean canExtract(int slot, ItemStack stack, Direction direction) {
-        return false;
-    }
+    public boolean canExtract(int slot, ItemStack stack, Direction direction) { return false; }
 }
