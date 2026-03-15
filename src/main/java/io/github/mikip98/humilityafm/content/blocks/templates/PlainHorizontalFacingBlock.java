d310ac6b4fc5d65c0464fe01736155848d9ca7cf
MikiP98 <pokora.mikolaj@gmail.com>
2026-03-15 19:33:41 +0100

    Small fix and import removal

--- src/main/java/io/github/mikip98/humilityafm/content/blocks/templates/PlainHorizontalFacingBlock.java
+++ src/main/java/io/github/mikip98/humilityafm/content/blocks/templates/PlainHorizontalFacingBlock.java
@@ -3,19 +3,17 @@ package io.github.mikip98.humilityafm.content.blocks.templates;
 #if MC_VERSION >= 12004
 import com.mojang.serialization.MapCodec;
 #endif
-import io.github.mikip98.humilityafm.content.blocks.cabinet.CabinetBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.NotNull;
 
 public class PlainHorizontalFacingBlock extends HorizontalFacingBlock {
     #if MC_VERSION >= 12004
-    protected static final MapCodec<CabinetBlock> CODEC = createCodec(CabinetBlock::new);
+    protected static final MapCodec<PlainHorizontalFacingBlock> CODEC = createCodec(PlainHorizontalFacingBlock::new);
 
     @Override
     protected MapCodec<? extends HorizontalFacingBlock> getCodec() {
@@ -25,18 +23,18 @@ public class PlainHorizontalFacingBlock extends HorizontalFacingBlock {
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(Properties.HORIZONTAL_FACING);
+        builder.add(FACING);
     }
 
     public PlainHorizontalFacingBlock(Settings settings) {
         super(settings);
         setDefaultState(getStateManager().getDefaultState()
-                .with(Properties.HORIZONTAL_FACING, Direction.SOUTH));
+                .with(FACING, Direction.SOUTH));
     }
 
     @Override
     public @NotNull BlockState getPlacementState(ItemPlacementContext ctx) {
         return this.getDefaultState()
-                .with(Properties.HORIZONTAL_FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+                .with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
     }
 }
